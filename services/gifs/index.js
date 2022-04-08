66a2c88ada6dfdcf0a1319899a3e89c2fc15bb5c
Anahi Gaetan <anahigaetan@gmail.com>
2022-04-08 20:11:09 -0300

    Fixed apis endpoints

--- services/gifs/index.js
+++ services/gifs/index.js
@@ -1,5 +1,6 @@
+const GIFS_LIMIT = 20;
 const API_URLS = {
-  TRENDING: "https://api.giphy.com/v1/gifs/trending?&limit=2",
+  TRENDING: `https://api.giphy.com/v1/gifs/trending?&limit=${GIFS_LIMIT}`,
 };
 
 const getTendenciesGifs = async () => {
@@ -7,7 +8,7 @@ const getTendenciesGifs = async () => {
     `${API_URLS.TRENDING}&api_key=${process.env.API_KEY}`
   );
   const json = await res.json();
-  return json;
+  return json.data;
 };
 
 export default getTendenciesGifs;
--- services/index.js
+++ services/index.js
@@ -1,7 +1,3 @@
-const URLS = {
-  TRENDING: "https://api.giphy.com/v1/gifs/trending?&limit=2",
-};
-
 const getUserName = async () => {
   const res = await fetch("http://localhost:3000/api/hello");
   const json = await res.json();
