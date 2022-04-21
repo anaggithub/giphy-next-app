df023c68583cd2d9ba8443bcc5f076540d943f74
Hadita <hadita@gmail.com>
2022-04-20 22:32:36 -0300

    Agrego el contenedor de Gifs en la pagina home

--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,44 @@
+import React, { useState } from "react";
 import Link from "next/link";
+import Card from "../components/Card";
 import getUserName from "../services";
 import getTendenciesGifs from "../services/gifs";
 import {
-  StyledContainer,
-  StyledTitle,
-  StyledAutor,
-  StyledLink,
-  StyledMain,
-  StyledSearchBox,
+ StyledContainer,
+ StyledTitle,
+ StyledAutor,
+ StyledLink,
+ StyledMain,
+ StyledSearchBox,
+ StyledGifContainer,
 } from "./styles";
-
+ 
 export default function Home({ userName, gifs }) {
-  console.log(gifs);
-  return (
-    <StyledContainer>
-      <StyledMain>
-        <StyledTitle>Giphy App</StyledTitle>
-        <StyledAutor>by {userName}</StyledAutor>
-        <StyledLink>
-          Link to <Link href="/detail">Details Page</Link>
-        </StyledLink>
-      </StyledMain>
-      <StyledSearchBox inputPlaceholder="Hola" />
-    </StyledContainer>
-  );
-}
 
+  const [selectedGifs, setSelectedGifs] = useState(gifs || []);
+
+ return (
+   <StyledContainer>
+     <StyledMain>
+       <StyledTitle>Giphy App</StyledTitle>
+       <StyledAutor>by {userName}</StyledAutor>
+       <StyledLink>
+         Link to <Link href="/detail">Details Page</Link>
+       </StyledLink>
+     </StyledMain>
+     <StyledSearchBox inputPlaceholder="Hola" />
+     <StyledGifContainer>
+       {selectedGifs.length &&
+         selectedGifs.map((gif) => (
+           <Card src={gif.images.original.url} alt={gif.title} key={gif.id} />
+         ))}
+     </StyledGifContainer>
+   </StyledContainer>
+ );
+}
+ 
 Home.getInitialProps = async () => {
-  const gifs = await getTendenciesGifs();
-  const userName = await getUserName();
-  return { gifs, userName };
+ const gifs = await getTendenciesGifs();
+ const userName = await getUserName();
+ return { gifs, userName };
 };
