3e7d5446aea94bd126a6f9c3bcba3d3bc01e03a5
Hadita <hadita@gmail.com>
2022-04-21 23:18:13 -0300

    Correcciones en la pagina de detalle

--- pages/detail/index.js
+++ pages/detail/index.js
@@ -1,13 +1,9 @@
-import Link from "next/link";
-import { StyledContainer } from "../styles";
+import { StyledContainer } from "./styles";
 
 export default function Detail() {
   return (
     <StyledContainer>
-      <h1>In progress</h1>
-      <p>
-        Link to <Link href="/">Home</Link>
-      </p>
+      <h1>Detail page in progress</h1>
     </StyledContainer>
   );
 }
---
+++ pages/detail/styles.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const StyledContainer = styled.div`
+  flex-grow: 1;
+  display: flex;
+  flex-direction: column;
+  padding: 0 30px;
+`;
