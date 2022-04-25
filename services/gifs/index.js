aadf00c67e872ab6251b9d34e2af63d197c514f9
Hadita <hadita@gmail.com>
2022-04-24 23:58:28 -0300

    Agrego api service para busqueda de gif por id

--- services/gifs/index.js
+++ services/gifs/index.js
@@ -2,6 +2,7 @@ const GIFS_LIMIT = 10;
 const API_URLS = {
   TRENDING: `https://api.giphy.com/v1/gifs/trending?&limit=${GIFS_LIMIT}`,
   SEARCH: `https://api.giphy.com/v1/gifs/search?&limit=${GIFS_LIMIT}`,
+  GET_BY_ID: `https://api.giphy.com/v1/gifs/`,
 };
 
 export const getTendenciesGifs = async () => {
@@ -15,6 +16,14 @@ export const getTendenciesGifs = async () => {
 export const getGifs = async (searchValue) => {
   const res = await fetch(
     `${API_URLS.SEARCH}&q=${searchValue}&api_key=${process.env.NEXT_PUBLIC_API_KEY}`
+  );
+  const json = await res.json();
+  return json;
+};
+
+export const getGifByID = async (id) => {
+  const res = await fetch(
+    `${API_URLS.GET_BY_ID}${id}?&api_key=${process.env.NEXT_PUBLIC_API_KEY}`
   );
   const json = await res.json();
   return json;
