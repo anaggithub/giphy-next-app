74aad8e9be2c139dc7e6b79169f07bb7cdc9c9bd
Hadita <hadita@gmail.com>
2022-04-24 23:59:54 -0300

    Elimino index de la pagina de detalle para que sea dinamica

--- pages/detail/index.js
+++
@@ -1,9 +0,0 @@
-import { StyledContainer } from "./styles";
-
-export default function Detail() {
-  return (
-    <StyledContainer>
-      <h1>Detail page in progress</h1>
-    </StyledContainer>
-  );
-}
