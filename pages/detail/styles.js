b0ee6ab15b8ae71a79fd8a5d4782d2f618be311e
Hadita <hadita@gmail.com>
2022-05-04 18:51:42 -0300

    Fixed styles in details page

--- pages/detail/styles.js
+++ pages/detail/styles.js
@@ -1,8 +1,18 @@
 import styled from "styled-components";
+import { colors, fontSizes } from "../../styles/variables";
 
 export const StyledContainer = styled.div`
   flex-grow: 1;
   display: flex;
   flex-direction: column;
-  padding: 0 30px;
+  align-items: center;
+  padding: 0 10%;
+`;
+
+export const StyledTitle = styled.h1`
+  display: flex;
+  flex-direction: column;
+  color: ${colors.lightBlue};
+  font-size: ${fontSizes.font18};
+  margin: 5% 0;
 `;
